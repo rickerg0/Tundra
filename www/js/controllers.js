8bb39862141c87b6a081b877c1b1ee039d9d9af1
Gregg Gaudin <gregg.gaudin@weatheranalytics.com>
2017-01-09 21:27:09 -0500

    refactored to start implementing the actual use cases

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,36 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,BLEService,Foo,$ionicModal) {
+.controller('DashCtrl', function($scope,BLEService,Foo,$ionicModal,$http) {
 	$scope.devices = [];
 	var isScanning = true;
 	$scope.myText =  "Scan";
 	$scope.myColor  ='danger';
 	document.getElementById("bleStatus").style= "color:green;";
 	$scope.startScanning = function () {
-		BLEService.connect(function(devices) {
-			$scope.devices = devices;	
+		BLEService.connect(function(exibitTags) {
+			$scope.exibitTags = exibitTags;
+			console.log(exibitTags);
 		});
 		
-		
 		$scope.myText =  "startScanning";
-		
 		console.log($scope.myText);
-		
-		$scope.myText =  "Scan again";
-		
 		isScanning = true;		
-		};
-	$scope.getResult = function (device) {
-		//$scope.device = device;
+	};
+	
+	$scope.getMedia = function (tag) {
 		Foo.getExhibitTag(function(data,status){
-			console.log(data.data);
-			if (data.data && data.data.length > 0) {
+			if (data.data) {
 				$scope.media=data.data
 			} else {
 				console.log("getResult: no data returned");
 				$scope.message = "no data returned"
 			}
-			},device );
+			},tag 
+		);
 		
 		$ionicModal.fromTemplateUrl('templates/getResults.html', {
 			  scope: $scope,
@@ -38,7 +34,8 @@ angular.module('starter.controllers', [])
 			 }).then(function(modal) {
 			     $scope.modal = modal;
 			     $scope.modal.show();
-			 });
+			 }
+		 );
 	};
 	
 	
@@ -46,8 +43,6 @@ angular.module('starter.controllers', [])
 		console.log("getOrganization");
 		Foo.getList(function(data,status){ 
 			$scope.list=data.data
-			//console.log(data);
-			//console.log($scope.list[0].name);
 			} );
 		
 	};
--- www/js/services.js
+++ www/js/services.js
@@ -5,9 +5,9 @@ angular.module('starter.services', [])
 	this.getList=function(callback){
 		$http({ url:"http://127.0.0.1:8080/TundraService/org/list" ,method:"GET"} ).then(callback,function(){ console.log("failed")});
 	}
-	this.getExhibitTag=function(callback,device){
-		console.log(device);
-		$http({ url:"http://127.0.0.1:8080/TundraService/tag/"+device.tag ,method:"GET"} ).then(callback,function(){ console.log("failed")});
+	this.getExhibitTag=function(callback,tag){
+		console.log(tag);
+		$http({ url:"http://127.0.0.1:8080/TundraService/tag/media/"+tag.exhibitTagTag ,method:"GET"} ).then(callback,function(){ console.log("failed")});
 	}
 
 	console.log("FOO created");
@@ -20,40 +20,57 @@ angular.module('starter.services', [])
 	var devices=[];
 	var that = this;
 	var onConnect = function(device) {
-		    device.name=device.name;
-			device.id=device.id;
-			device.rssi=device.rssi;
-			
-			devices.push(device);
-			return devices;
-				 
+	    device.name=device.name;
+		device.id=device.id;
+		device.rssi=device.rssi;
+		devices.push(device);
+
+		return devices;
 	 }
 	var onError = function() {
-		    device.name='none';
-			device.id='0';
-			device.rssi='0';
-			
-			
-			
-			return devices;	 
+	    device.name='none';
+		device.id='0';
+		device.rssi='0';
+		
+		return devices;	 
 	 }
    
 	return {
 		connect:function(callback) {
+			var devices=[];
 			if (typeof(ble) != "undefined") {
 				ble.scan([], 30, onConnect, onError);
-						
-					callback(devices);
+				var exibitTags=[];
+				for (var i = 0; i < devices.length; i++) {
+					var d = devices[i];
+					console.log(d);
+					$http({ url:"http://127.0.0.1:8080/TundraService/tag/"+d.tag ,method:"GET"} ).then(function(data,status) {
+						for (var i = 0; i< data.data.length; i++) {
+							exibitTags.push({organizationName:data.data[i].organizationName,locationName:data.data[i].locationName,exhibitTagName:data.data[i].exhibitTagName,exhibitTagId:data.data[i].exhibitTagId,exhibitTagTag:data.data[i].exhibitTagTag,exhibitTagMimeType:data.data[i].exhibitTagMimeType});
+						}
+						callback(exibitTags);
+					},function(){ console.log("failed")});
+				}
 			} else {
 				$http({ url:"http://127.0.0.1:8080/TundraService/tag/list" ,method:"GET"} ).then(function(data,status) {
 					
 					for (var i = 0; i< data.data.length; i++) {
 						devices.push({name:data.data[i].name,id:data.data[i].id,tag:data.data[i].tag,rssi:i});
 					}
-					callback(devices);
+
+					var exibitTags=[];
+					for (var i = 0; i < devices.length; i++) {
+						var d = devices[i];
+						$http({ url:"http://127.0.0.1:8080/TundraService/tag/"+d.tag ,method:"GET"} ).then(function(data,status) {
+							exibitTags.push({organizationName:data.data.organizationName,locationName:data.data.locationName,exhibitTagName:data.data.exhibitTagName,exhibitTagId:data.data.exhibitTagId,exhibitTagTag:data.data.exhibitTagTag,exhibitTagMimeType:data.data.exhibitTagMimeType});
+							callback(exibitTags);
+						},function(){ console.log("failed")});
+					}
 				},function(){ console.log("failed")});
 
-			} 
+			}
+			
+			
 		},
 		disconnect:function(device) {
 			
